edb0023f9c5ad01509028aa5d8693d3c6b82f1ca
BROHIT-cmd <Bharat.rohit@xylem.com>
2026-07-03 14:57:15 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,42 @@
+PIPE_DATA = {
+    "DN100": {"od": 114},
+    "DN150": {"od": 168},
+    "DN200": {"od": 219},
+    "DN250": {"od": 273},
+    "DN300": {"od": 324}
+}
+
+SUPPORT_SPACING = {
+    "DN100": 2.5,
+    "DN150": 3.0,
+    "DN200": 3.5,
+    "DN250": 4.0,
+    "DN300": 4.5
+}
+
+ANCHORS = {
+    "M16": {
+        "hole":18,
+        "slot_width":18,
+        "slot_length":38,
+        "embedment":200
+    },
+    "M20": {
+        "hole":22,
+        "slot_width":22,
+        "slot_length":42,
+        "embedment":250
+    },
+    "M24": {
+        "hole":26,
+        "slot_width":26,
+        "slot_length":46,
+        "embedment":300
+    },
+    "M30": {
+        "hole":33,
+        "slot_width":33,
+        "slot_length":53,
+        "embedment":375
+    }
+}
